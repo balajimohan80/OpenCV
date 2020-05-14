8c465dc8bef45f95de856531561c9c1ff6af536d
balajimohan80 <64716848+balajimohan80@users.noreply.github.com>
2020-05-14 08:08:10 -0700

    Creating Image memory using CV::Mat

--- Image_Operation/Image_Create/Image_Create.cpp
+++ Image_Operation/Image_Create/Image_Create.cpp
@@ -5,6 +5,22 @@
 #include <opencv2/highgui.hpp>
 
 int main() {
-	std::cout << "Hello multiple proj\n";
+	std::cout << "Creating an Empty Matrix\n";
+	//Scalar value zero means Pure-Black
+	cv::Mat EmptyMatrix = cv::Mat(100, 200, CV_8UC3, cv::Scalar(0, 0, 0));
+	const std::string W_Name = "EmptyMatrix";
+	cv::namedWindow(W_Name, 1);
+	cv::imshow(W_Name, EmptyMatrix);
+	cv::waitKey(0);
+	//Change all Pixels color to pure-white, by setting all pixel value to 255
+	EmptyMatrix.setTo(cv::Scalar(255, 255, 255));
+	cv::imshow(W_Name, EmptyMatrix);
+	cv::waitKey(0);
+	//Change all Pixels color to 127
+	EmptyMatrix.setTo(cv::Scalar(127, 127, 127));
+	cv::imshow(W_Name, EmptyMatrix);
+	cv::waitKey(0);
+	cv::destroyAllWindows();
+	std::cout << "Exit...\n";
 	return 0;
 }
